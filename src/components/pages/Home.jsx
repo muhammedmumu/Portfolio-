294885c0f5a7fc4e31bf02ff85e12eeeb73b1398
Muhammed Mukrim <muhammedmukrim>
2026-04-24 18:37:50 +0530

    Fix Home component height for improved layout consistency

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sunny from '../../assets/Thousand-Sunny-One-Piece-464.png'
 export default function Home() {
   return (
     <div id='home' className='home-mobile-balance relative min-h-screen w-screen scroll-mt-24 overflow-hidden bg-[#076585]'>
-      <div className='home-mobile-hero relative h-[82vh] bg-[#ff4700] pt-16 sm:pt-20 lg:h-[86vh] lg:pt-24'>
+      <div className='home-mobile-hero relative h-[82vh] bg-[#ff4700] pt-16 sm:pt-20 lg:h-[75vh] lg:pt-24'>
         <div className='flex h-1/2 items-center justify-center px-4'>
           <Name />
         </div>
